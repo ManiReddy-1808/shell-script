066231265aea68b53f9fa53f1808f2eac82eac03
ManiReddy-1808 <manireddy18082002@gmail.com>
2024-11-25 11:39:55 +0530

    Added new line

---
+++ 2.conversation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Mani:: Hello Suresh, How are you?"
+echo "Jagan:: Hi Mani, I am fine. How are you?"
+echo "Mani: I am fine too. how is your work?"
+echo "Jagan:: not bad. I am thinking to upgrade to DevOps"
+
+# 1. Repeated Words
+# 2. Tf you want to change, you have to change it every where
+# 3. While changing accidently you may change actual code
+
+# Solution?  use variable
---
+++ 3.variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PERSON1=MANI
+PERSON2=JAGAN
+
+echo "$PERSON1:: Hello Suresh, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
+echo "$PERSON1: I am fine too. how is your work?"
+echo "$PERSON2:: not bad. I am thinking to upgrade to DevOps"
+
+# 1. Repeated Words
+# 2. Tf you want to change, you have to change it every where
+# 3. While changing accidently you may change actual code
+
+# Solution?  use variable
