e869cc08b570d858fd2eb70223f6b7266cc4d5b1
ManiReddy-1808 <manireddy18082002@gmail.com>
2026-01-29 18:44:36 +0530

    Added new code

--- 08-Numbers.sh
+++ 08-Numbers.sh
@@ -5,8 +5,8 @@ NUM2=$2
 
 echo "The sum of $NUM1 + $NUM2 is: $((NUM1+NUM2))"
 
-NAMES=("Mani", "Kanta", "Reddy")
+NAMES=("Mani" "Kanta" "Reddy")
 
-echo "Name are: $NAMES[@]"
-echo "First Name is: $NAMES[0]"
-echo "Last Name is: $NAMES[2]"
+echo "Name are: ${NAMES[@]}"
+echo "First Name is: ${NAMES[0]}"
+echo "Last Name is: ${NAMES[2]}"
