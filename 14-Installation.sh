e440aae360ad34b60f4e5c85b2fa2fdc14be37e1
ManiReddy-1808 <manireddy18082002@gmail.com>
2026-01-29 21:23:35 +0530

    Added new code

---
+++ 14-Installation.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+
+if [ $USER_ID -gt 0 ]; then
+    echo "Please run this script with root user :)"
+    exit 3;
+fi
+
+mkdir -p $LOGS_FOLDER
+
+# tee command is used to write the output to a file as well as to the console
+VALIDATE(){  
+    if [ $1 -eq 0 ]; then
+        echo "$2 ... SUCCESS" | tee -a $LOGS_FILE
+    else 
+        echo "$2 ... FAILURE" | tee -a $LOGS_FILE
+fi
+}
+
+for package in $@ # Call: sh 14-Install.sh nginx mysql nodejs
+do
+    dnf install $package  -y
+    VALIDATE $? "$package installation"
+done
