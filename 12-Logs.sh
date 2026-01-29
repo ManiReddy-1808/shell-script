9e130c1c941521beb086d6d0df754c76bc45a2b1
ManiReddy-1808 <manireddy18082002@gmail.com>
2026-01-29 21:07:19 +0530

    Added new code

--- 12-Logs.sh
+++ 12-Logs.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 USER_ID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
 
 if [ $USER_ID -gt 0 ]; then
     echo "Please run this script with root user :)"
     exit 3;
 fi
+
+mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
     if [ $1 -eq 0 ]; then
@@ -15,12 +19,11 @@ VALIDATE(){
 fi
 }
 
-dnf install nginx -y
+dnf install nginx -y &>>$LOGS_FILE
 VALIDATE $? "Installing Nginx"
 
-dnf install mysql -y
+dnf install mysql -y &>>$LOGS_FILE
 VALIDATE $? "Installing MYSQL"
 
-
-
-
+dnf install nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Installing NodeJS"
