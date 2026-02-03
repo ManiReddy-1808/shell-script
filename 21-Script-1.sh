e629369853bb62c7e00b82bbf2c3b383d2389dec
ManiReddy-1808 <manireddy18082002@gmail.com>
2026-02-03 11:15:26 +0530

    Added new code

--- 21-Script-1.sh
+++ 21-Script-1.sh
@@ -6,4 +6,4 @@ echo "Name is: $NAME"
 echo "PID of Script-1: $$"
 
 #sh 22-Script-2.sh   # Seperate PID, can't access other script variables
-./22-Script-2.sh     # Same PID, CAN access other scrit variables
+source ./22-Script-2.sh     # Same PID, CAN access other scrit variables (source)
